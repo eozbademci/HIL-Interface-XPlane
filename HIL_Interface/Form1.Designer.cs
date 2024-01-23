4b2e3eb229973be50961e33e75a2b8a65b01d90f
Erdal Özbademci <b200109036@subu.edu.tr>
2024-01-23 16:00:38 +0300

    add modules and interface editing

--- HIL_Interface/Form1.Designer.cs
+++ HIL_Interface/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HIL_Interface
 {
-    partial class Form1
+    partial class UserInterface
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserInterface));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.targetAddressTextBox = new System.Windows.Forms.TextBox();
+            this.targetPortTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.YellowGreen;
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.connectButton.Location = new System.Drawing.Point(562, 398);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(79, 34);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "Bağlan";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.BackColor = System.Drawing.Color.Red;
+            this.disconnectButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.disconnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.disconnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.disconnectButton.Location = new System.Drawing.Point(658, 398);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(130, 34);
+            this.disconnectButton.TabIndex = 4;
+            this.disconnectButton.Text = "Bağlantıyı Kes";
+            this.disconnectButton.UseVisualStyleBackColor = false;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(253, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hedef IP Adres:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // targetAddressTextBox
+            // 
+            this.targetAddressTextBox.BackColor = System.Drawing.SystemColors.GrayText;
+            this.targetAddressTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.targetAddressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.targetAddressTextBox.Location = new System.Drawing.Point(356, 408);
+            this.targetAddressTextBox.Name = "targetAddressTextBox";
+            this.targetAddressTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.targetAddressTextBox.Size = new System.Drawing.Size(105, 17);
+            this.targetAddressTextBox.TabIndex = 2;
+            this.targetAddressTextBox.Text = "10.10.10.15";
+            this.targetAddressTextBox.TextChanged += new System.EventHandler(this.targetAddressTextBox_TextChanged);
+            // 
+            // targetPortTextBox
+            // 
+            this.targetPortTextBox.BackColor = System.Drawing.SystemColors.GrayText;
+            this.targetPortTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.targetPortTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.targetPortTextBox.Location = new System.Drawing.Point(502, 408);
+            this.targetPortTextBox.Name = "targetPortTextBox";
+            this.targetPortTextBox.Size = new System.Drawing.Size(59, 17);
+            this.targetPortTextBox.TabIndex = 2;
+            this.targetPortTextBox.Text = "22222";
+            this.targetPortTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(467, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.targetPortTextBox);
+            this.Controls.Add(this.targetAddressTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserInterface";
+            this.Text = "User Interface";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox targetAddressTextBox;
+        private System.Windows.Forms.TextBox targetPortTextBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- HIL_Interface/Form1.cs
+++ HIL_Interface/Form1.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using MathWorks.xPCTarget.FrameWork;
+
 
 namespace HIL_Interface
 {
-    public partial class Form1 : Form
+    public partial class UserInterface : Form
     {
-        public Form1()
+        public UserInterface()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void targetAddressTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Before trying to connect, make sure we're disconnected first in case we are now connecting to a different target.
+                if (tg.IsConnected == true)
+                {
+                    tg.Disconnect();
+                    if (tg.IsConnected != false)
+                        MessageBox.Show("Could not Disconnect!");
+                }
+
+                // Set the target TCP/IP address and port.
+                tg.TcpIpTargetAddress = targetAddressTextBox.Text;
+                tg.TcpIpTargetPort = "22222";
+
+                // Now connect.
+                tg.Connect();
+                if (tg.IsConnected == true)
+                {
+                    MessageBox.Show("Connected!");
+
+                    connectButton.Enabled = false;
+                    loadButton.Enabled = true;
+                    startButton.Enabled = false;
+                    stopButton.Enabled = false;
+                    unloadButton.Enabled = false;
+                    disconnectButton.Enabled = true;
+                }
+                else
+                    MessageBox.Show("Could not Connect!");
+            }
+            catch (xPCException me)
+            {
+                MessageBox.Show(me.Message);
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Disconnect from the target.
+                tg.Disconnect();
+                if (tg.IsConnected == false)
+                {
+                    MessageBox.Show("Disconnected!");
+
+                    connectButton.Enabled = true;
+                    loadButton.Enabled = false;
+                    startButton.Enabled = false;
+                    stopButton.Enabled = false;
+                    unloadButton.Enabled = false;
+                    disconnectButton.Enabled = false;
+                }
+                else
+                    MessageBox.Show("Could not Disconnect!");
+            }
+            catch (xPCException me)
+            {
+                MessageBox.Show(me.Message);
+            }
         }
     }
 }
